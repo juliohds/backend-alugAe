db65d705717e34c22bcf6b6639819bd41399e139
juliohds <tecjuliohenrique@gmail.com>
2018-10-21 10:55:38 -0300

    crawler

--- app/Http/Controllers/CrawlerUser.php
+++ app/Http/Controllers/CrawlerUser.php
@@ -14,15 +14,39 @@ class CrawlerUser extends Controller{
         if($html != null){
 
             
-                //$array = explode("-",$phase);
+            static $seen = array();
+            if (isset($seen[$url]) || $depth === 0) {
+                return;
+            }
+        
+            $seen[$url] = true;
+        
+            
+            $anchors = $dom->getElementsByTagName('a');
+            foreach ($anchors as $element) {
+                $href = $element->getAttribute('href');
+                if (0 !== strpos($href, 'http')) {
+                    $path = '/' . ltrim($href, '/');
+                    if (extension_loaded('http')) {
+                        $href = http_build_url($url, array('path' => $path));
+                    } else {
+                        $parts = parse_url($url);
+                        $href = $parts['scheme'] . '://';
+                        if (isset($parts['user']) && isset($parts['pass'])) {
+                            $href .= $parts['user'] . ':' . $parts['pass'] . '@';
+                        }
+                        $href .= $parts['host'];
+                        if (isset($parts['port'])) {
+                            $href .= ':' . $parts['port'];
+                        }
+                        $href .= dirname($parts['path'], 1).$path;
+                    }
+                }
+                crawl_page($href, $depth - 1);
+            }
+            echo "URL:",$url,PHP_EOL,"CONTENT:",PHP_EOL,$dom->saveHTML(),PHP_EOL,PHP_EOL;
 
-                //  $new = new Noticia;
-                //  $new->titulo = utf8_encode(str_replace("&nbsp;","",$array[1]));
-                //  $new->conteudo = utf8_encode(str_replace("&nbsp;","",$array[1]));
-                //  $new->data = str_replace("&nbsp;","",$array[0]);
-                //  $new->save();
-
-            return $html->find("#pageTitle");
+            return $html->find(".g47SY");
         }
     }
 
