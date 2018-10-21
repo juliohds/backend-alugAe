50d12eff2f0d45adcf236f0806664d18dcd52738
juliohds <tecjuliohenrique@gmail.com>
2018-10-21 02:55:26 -0300

    crud endereco

--- app/Endereco.php
+++ app/Endereco.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Endereco extends Model
 {
-    //
+    protected $fillable = [
+        'rua', 'bairro', 'cidade', 'pais', 'numero',
+    ];
 }
--- app/Http/Controllers/EnderecoController.php
+++ app/Http/Controllers/EnderecoController.php
@@ -4,81 +4,43 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Endereco;
+
 class EnderecoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function showAll()
     {
-        //
+        $endereco = Endereco::all();
+        return response()->json($endereco);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function showById($id)
     {
-        //
+        $endereco = Endereco::find($id);
+        return response()->json($endereco);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function create(Request $request)
     {
-        //
+        $endereco = new Endereco();
+        $endereco->fill($request->all());
+        $endereco->save();
+        return response()->json($endereco);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        //
+        $endereco = Endereco::find($id);
+        $endereco->fill($request->all());
+        $endereco->update();
+        return response()->json($endereco);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+    public function delete($id)
     {
-        //
+        $endereco = Endereco::find($id);
+        $endereco->delete();
+        return response()->json($endereco);
     }
+
 }
