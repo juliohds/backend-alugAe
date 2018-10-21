c2db13ac3b50766507561984a55cf6da11ba413f
juliohds <tecjuliohenrique@gmail.com>
2018-10-21 03:34:55 -0300

    todos os cruds prontos

--- app/Http/Controllers/LocacoesController.php
+++ app/Http/Controllers/LocacoesController.php
@@ -2,9 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Locacoes;
 use Illuminate\Http\Request;
 
 class LocacoesController extends Controller
 {
-    //
+    public function showAll()
+    {
+        $locacoes = Locacoes::all();
+        return response()->json($locacoes);
+    }
+
+    public function showById($id)
+    {
+        $locacoes = Locacoes::find($id);
+        return response()->json($locacoes);
+    }
+
+    public function create(Request $request)
+    {
+        $locacoes = new Locacoes();
+        $request->nome;
+        $locacoes->fill($request->all());
+        $locacoes->save();
+        return response()->json($locacoes);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $locacoes = Locacoes::find($id);
+        $locacoes->fill($request->all());
+        $locacoes->update();
+        return response()->json($locacoes);
+    }
+
+    public function delete($id)
+    {
+        $locacoes = Locacoes::find($id);
+        $locacoes->delete();
+        return response()->json($locacoes);
+    }
 }
--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -3,8 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Perfil;
 
 class PerfilController extends Controller
 {
-    //
+    public function showAll()
+    {
+        $perfil = Perfil::all();
+        return response()->json($perfil);
+    }
+
+    public function showById($id)
+    {
+        $perfil = Perfil::find($id);
+        return response()->json($perfil);
+    }
+
+    public function create(Request $request)
+    {
+        $perfil = new Perfil();
+        $request->nome;
+        $perfil->fill($request->all());
+        $perfil->save();
+        return response()->json($perfil);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $perfil = Perfil::find($id);
+        $perfil->fill($request->all());
+        $perfil->update();
+        return response()->json($perfil);
+    }
+
+    public function delete($id)
+    {
+        $perfil = Perfil::find($id);
+        $perfil->delete();
+        return response()->json($perfil);
+    }
 }
